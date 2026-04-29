2c422eaa3f7ae617634a13172c76a700d2a78088
Gema <gema72918@gmail.com>
2026-04-29 08:26:27 +0700

    second commmit

--- tugas-java/kubus.js
+++ tugas-java/kubus.js
@@ -1,11 +1,11 @@
 const prompt = require("prompt-sync")({ sigint: true })
 let sisi = Number(prompt('Masukan sisi: '))
-let luas = sisi * sisi
-let keliling = 4 * sisi
+let volume = sisi * sisi * sisi
+let luasPermukaan = 6 * sisi * sisi
 
 console.log("========================")
 console.log("         HASIL          ")
 console.log("========================")
 
-console.log(`Luas persegi\t\t: ${luas.toFixed(2)} cm2`)
-console.log(`Keliling persegi\t: ${keliling.toFixed(2)} cm`)
+console.log(`Volume kubus\t\t: ${volume.toFixed(2)} cm3`)
+console.log(`Luas permukaan kubus\t: ${luasPermukaan.toFixed(2)} cm2`)
